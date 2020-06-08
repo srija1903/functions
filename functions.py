2d86d7b344dd8c7843ed1b8e38dce48fa144ae8c
srija1903 <64838154+srija1903@users.noreply.github.com>
2020-06-08 14:05:45 +0530

    Create functions.py

---
+++ functions.py
@@ -0,0 +1,14 @@
+list1=[12,-7,5,64,-14]
+list2=[12,14,-95,3]
+list1.append(23)
+list2.append(2)
+print(list1)
+print(list2)
+tuple1=(1,3,4,5,-10,11)
+print(tuple1)
+print(tuple1[2])
+print(tuple1[2:5])
+dict={"C":"Easy","C++":"Moderate","Java":"Hard","Python":"Cool"}
+del dict["Python"]
+dict.pop("C++")
+print(dict)
